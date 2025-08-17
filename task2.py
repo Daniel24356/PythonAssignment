8c01162c33f912cc63bca28214076b7d2e36af9c
Daniel24356 <igwedaniel128@gmail.com>
2025-08-17 13:48:32 -0700

    added rank students in task2.py

--- task2.py
+++ task2.py
@@ -18,3 +18,17 @@ def groupAnagrams(sentences):
 
 
 print(groupAnagrams(['Listen to me', 'Enlist to me', 'The eyes', 'They see']))
+
+#4
+def rankStudentsByAvgScore(dicts):
+    students = []
+    for name, scores in dicts.items():
+        #find the avg of the values
+        avg = sum(scores) / len(scores)
+        students.append((name, round(avg, 2)))
+    
+    #sort by descending order (-x[1]) and if avg is equal sort name by aplabetical order(x[0])
+    sorted_students = sorted(students, key=lambda x: (-x[1], x[0]))
+    return sorted_students
+
+print(rankStudentsByAvgScore({'Alice':[90,85,88], 'Bob':[90,85,88], 'Charlie':[95,80,85]}))
